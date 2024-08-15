42797894171a6e4d463b7e579b3664f7e4c3688d
Carl Peto <CPeto@becrypt.com>
2024-08-15 10:08:31 +0100

    correct hal package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["serial"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/microswift-packages/hal", from: "1.0.0")
+        .package(url: "https://github.com/microswift-packages/hal-baseline", from: "1.0.0")
     ],
     targets: [
         .target(
